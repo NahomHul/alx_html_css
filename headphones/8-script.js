d08e811907fa9f97a8e292a47b8c03fe2a2657b0
Nahom Hul <nahomhulum@gmail.com>
2025-08-09 15:57:20 +0300

    DOM manipulation

---
+++ headphones/8-script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+  hamburger.setAttribute('aria-expanded', !expanded);
+  navLinks.classList.toggle('open');
+});
